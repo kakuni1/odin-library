30f3bb68f6e1a616c054d2cf3a0380c26bf550e7
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-03-12 22:26:53 -0700

    add: div creation logic

--- library.js
+++ library.js
@@ -1,20 +1,46 @@
 const myLibrary = [];
 
-function Book(title, author, id) {
-  this.author = author;
+function Book(title, id, authorNameLast, authorNameFirst) {
   this.title = title;
   this.id = id;
+  this.authorNameLast = authorNameLast;
+  this.authorNameFirst = authorNameFirst;
+
   this.printTitle = function () {
-    return `title: ${this.title} \nby: ${this.author} \nid: id${this.id}`;
+    return `title: ${this.title} \nby: ${this.authorNameFirst} ${this.authorNameLast} \nid: id${this.id}`;
   };
 }
 
-const bookFirstEntry = new Book("a1", "author one", crypto.randomUUID());
+const bookFirstEntry = new Book(
+  "Book of John",
+  crypto.randomUUID(),
+  "Doe",
+  "John",
+);
+const bookSecondEntry = new Book(
+  "Book of Jane",
+  crypto.randomUUID(),
+  "Doe",
+  "Jane",
+);
+const bookThirdEntry = new Book(
+  "Book of Smith",
+  crypto.randomUUID(),
+  "Smith",
+  "John",
+);
 
 myLibrary[0] = bookFirstEntry;
-console.log(myLibrary[0]);
+myLibrary[1] = bookSecondEntry;
+myLibrary[2] = bookThirdEntry;
 
-const container = document.querySelector(".container");
+const shelf = document.querySelector(".shelf");
 const div = document.createElement("div");
-div.classList.add("library");
-container.appendChild(div);
+div.className = "book";
+document.querySelector(".shelf").appendChild(div);
+
+myLibrary.forEach((book) => {
+  const bookDiv = document.createElement("div");
+  bookDiv.textContent = book.title;
+  document.querySelector(".book").appendChild(bookDiv);
+});
