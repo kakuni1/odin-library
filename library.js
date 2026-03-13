d3cf05ca36306b3d644d5d4757b8cea8042904eb
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-03-12 17:08:51 -0700

    add: id & testing

--- library.js
+++ library.js
@@ -1,9 +1,18 @@
 const myLibrary = [];
 
-function Book(title, author) {
-  this.title = title;
+function Book(title, author, id) {
   this.author = author;
+  this.title = title;
+  this.id = id;
   this.printTitle = function () {
-    return `title: ${this.title}, by: ${this.author}`;
+    return `title: ${this.title} \nby: ${this.author} \nid: id${this.id}`;
   };
 }
+
+const bookA1 = new Book("a1", "author one", crypto.randomUUID());
+const bookA2 = new Book("a2", "author two", crypto.randomUUID());
+const bookB1 = new Book("b1", "author three", crypto.randomUUID());
+
+console.log(bookA1.printTitle());
+console.log(bookA2.printTitle());
+console.log(bookB1.printTitle());
